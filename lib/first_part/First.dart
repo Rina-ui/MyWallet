bae85fba852fc511d6c9d243faa930cd8398994f
Rina-ui <marinagracia895@gmail.com>
2025-06-09 15:05:28 +0200

    first page

--- lib/first_part/First.dart
+++ lib/first_part/First.dart
@@ -1,31 +1,80 @@
 import 'package:flutter/material.dart';
 
-class First extends StatelessWidget {
+class First extends StatefulWidget {
   const First({super.key});
+
+  @override
+  State<First> createState() => _FirstState();
+}
+
+class _FirstState extends State<First> {
+
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => First()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
           body: Center(
-            child: Row(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Container(
-                  height: 200,
                   width: 200,
+                  height: 200,
                   decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                    gradient: RadialGradient(
-                        colors: [
-                          Color(0xFFFFFFFF),
-                          Colors.grey.shade300,
-                        ],
-
-                      center: Alignment.center,
-                      radius: 0.9,
-                    )
+                    shape: BoxShape.circle,
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Colors.white,
+                        Color(0xFFB0B3C6),
+                        Color(0xFF100D40),
+                      ],
+                    ),
                   ),
+                  child: Center(
+                    child: Text(
+                      'My Wallet',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF100D40),
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 20,),
+
+                Text('My Wallet — All Your Money, One Smart Place',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Color(0xFF100D40),
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+
+                SizedBox(height: 25,),
+
+                Text('All Your Subscriptions. One Secure Wallet!',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Color(0xFF100D40),
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w400,
+                    )
                 )
               ],
             ),
